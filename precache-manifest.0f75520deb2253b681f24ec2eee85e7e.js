62755fc6796bb16f181a839f1b648bf4c32c9e8c
Hung Nguyen <happyhung@yahoo.com>
2020-05-28 02:00:58 +0300

    Updates

---
+++ precache-manifest.0f75520deb2253b681f24ec2eee85e7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfbe2909c7c6e859883b775a2cf48eb6",
+    "url": "/react-redux-countries-app/index.html"
+  },
+  {
+    "revision": "5f552387ac1b2e85aa63",
+    "url": "/react-redux-countries-app/static/js/2.9cc876e2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-redux-countries-app/static/js/2.9cc876e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f06c515c1ef651d5ba83",
+    "url": "/react-redux-countries-app/static/js/main.6a84b7da.chunk.js"
+  },
+  {
+    "revision": "9a54034e3a045c0bf16c",
+    "url": "/react-redux-countries-app/static/js/runtime-main.ea4f1432.js"
+  }
+]);
